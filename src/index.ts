92893e122c1d924fed2d59496e2907e9d1066182
Sampath <sampathnadeesh0329@gmail.com>
2025-11-24 12:05:15 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://rad-sample-be.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
 )
